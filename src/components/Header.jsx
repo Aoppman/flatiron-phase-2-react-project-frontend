cce8de6f6a8b33afbc99cf07ad92aa9cb3e3d42e
Andrew <a.oppman@gmail.com>
2023-10-08 14:48:29 -0400

    components folder added ; Header component file/initial code added

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  <>
+    <header>
+      <h1>My App</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#some-id">Some Link Title</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </>;
+}
+
+export default Header;
